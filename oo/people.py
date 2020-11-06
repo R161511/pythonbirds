9cd491fec0fe34ed0068e9f8b78e19c459d7e77e
Rafael <rafyangtj@gmail.com>
2020-11-06 15:43:09 -0300

    Created instance atribute - name and age

--- oo/people.py
+++ oo/people.py
@@ -1,13 +1,21 @@
 # Create the Class, who define how the objects enviroment
 
 class People: # OBJETO
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self): # PARAMETRO
         return f'\033[30mHello\033[m{id(self)}'
 
 if __name__ == '__main__':
-    p = People()
+    p = People('Aline')
     print(People.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "\033[33mRafael\033[m"
+    print(p.nome)
+    print(p.idade)
 
 
