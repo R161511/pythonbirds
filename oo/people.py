b76bd9cd93e8fc7477580a8f2d0e0dd5784b5b60
Rafael <rafyangtj@gmail.com>
2020-11-06 13:13:36 -0300

    Created greet method

--- oo/people.py
+++ oo/people.py
@@ -1,4 +1,13 @@
 # Create the Class, who define how the objects enviroment
 
-class People:
-    pass
+class People: # OBJETO
+    def cumprimentar(self): # PARAMETRO
+        return f'\033[30mHello\033[m{id(self)}'
+
+if __name__ == '__main__':
+    p = People()
+    print(People.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
+
