57d34f02dea9c329f63899025ab6b06157beee29
Rafael <rafyangtj@gmail.com>
2020-11-07 13:46:35 -0300

    Created class eyes atribute

--- oo/people.py
+++ oo/people.py
@@ -1,6 +1,8 @@
 # Create the Class, who define how the objects enviroment
 
 class People: # OBJETO
+    eyes = 2 # Atribute de classe ou default
+
     def __init__(self, *sons, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -21,8 +23,16 @@ if __name__ == '__main__':
         print(sons.nome)
     aline.sobrenome = "Reis"
     del aline.sons
+    aline.eyes = 1
+    del aline.eyes
     print(aline.__dict__)
     print(rafael.__dict__)
+    People.eyes = 3
+    print(People.eyes)
+    print(aline.eyes)
+    print(rafael.eyes)
+    print(id(People.eyes), id(aline.eyes), id(rafael.eyes))
+
 
 
 
